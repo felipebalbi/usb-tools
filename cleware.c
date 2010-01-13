f5acc8c68a7f1882aca4058cad7290e878e4cf60
Felipe Balbi <felipe.balbi@nokia.com>
2010-01-14 00:45:36 +0200

    usb-tools: cleware: fix up file license header
    Make it consistent with what we use on the project.
    
    Signed-off-by: Felipe Balbi <felipe.balbi@nokia.com>

--- cleware.c
+++ cleware.c
@@ -2,20 +2,22 @@
 /**
  * cleware.c - Cleware USB-Controlled Power Switch
  *
- * Copyright (C) 2010 - Felipe Balbi <felipe.balbi@nokia.com>
+ * Copyright (C) 2010 Felipe Balbi <felipe.balbi@nokia.com>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * This file is part of the USB Verification Tools Project
+ *
+ * USB Tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public Liicense as published by
+ * the Free Software Foundation, either version 3 of the license, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * USB Tools is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with USB Tools. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdio.h>
