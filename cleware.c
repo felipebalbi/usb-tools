8c962cd9efbc4af40c47d4269737c782fda0da42
Felipe Balbi <felipe.balbi@nokia.com>
2010-01-15 17:48:24 +0200

    usb-tools: cleware: iterate over devices and find suitable one
    we might want to have several cleware usb-switch attached
    via a hub. We now allow users to pass the device's serial
    number as the second argument to the tool so that we can
    talk to the correct cleware device. If serial number isn't
    passed, we will open the first found one.
    
    Signed-off-by: Felipe Balbi <felipe.balbi@nokia.com>

--- cleware.c
+++ cleware.c
@@ -31,6 +31,21 @@
 #define CLEWARE_VENDOR_ID	0x0d50
 #define CLEWARE_PRODUCT_ID	0x0008
 
+struct usb_device_id {
+	unsigned		idVendor;
+	unsigned		idProduct;
+};
+
+#define USB_DEVICE(v, p)		\
+{					\
+	.idVendor		= v,	\
+	.idProduct		= p,	\
+}
+
+static struct usb_device_id cleware_id[] = {
+	USB_DEVICE(CLEWARE_VENDOR_ID, CLEWARE_PRODUCT_ID),
+};
+
 static int debug;
 
 #define DBG(fmt, args...)			\
@@ -39,6 +54,116 @@ static int debug;
 
 #define TIMEOUT			1000	/* ms */
 #define ARRAY_SIZE(x)	(sizeof(x) / sizeof((x)[0]))
+
+static int match_device_id(libusb_device *udev)
+{
+	struct libusb_device_descriptor	desc;
+	int				match = 0;
+	int				ret;
+	int				i;
+
+	ret = libusb_get_device_descriptor(udev, &desc);
+	if (ret < 0) {
+		DBG("%s: failed to get device descriptor\n", __func__);
+		return ret;
+	}
+
+	for (i = 0; i < ARRAY_SIZE(cleware_id); i++) {
+		DBG("%s: idVendor %02x idProduct %02x\n",__func__,
+				desc.idVendor, desc.idProduct);
+		if ((desc.idVendor == cleware_id[i].idVendor) &&
+				(desc.idProduct == cleware_id[i].idProduct))
+			match = 1;
+	}
+
+	return match;
+}
+
+static int match_device_serial_number(libusb_device *udev, unsigned iSerial)
+{
+	struct libusb_device_descriptor	desc;
+
+	libusb_device_handle		*tmp;
+
+	unsigned char			serial;
+	int				ret;
+
+	ret = libusb_open(udev, &tmp);
+	if (ret < 0 || !tmp) {
+		DBG("%s: couldn't open device\n", __func__);
+		goto out0;
+	}
+
+	if (libusb_kernel_driver_active(tmp, 0)) {
+		ret = libusb_detach_kernel_driver(tmp, 0);
+		if (ret < 0) {
+			DBG("%s: couldn't detach kernel driver\n", __func__);
+			goto out1;
+		}
+	}
+
+	ret = libusb_get_string_descriptor(tmp, desc.iSerialNumber,
+			0x0409, &serial, sizeof(serial));
+
+	if (ret < 0) {
+		DBG("%s: failed to get string descriptor\n", __func__);
+		goto out1;
+	}
+
+	if (serial != iSerial) {
+		DBG("%s: not the serial number we want\n", __func__);
+		goto out2;
+	}
+
+out2:
+	libusb_attach_kernel_driver(tmp, 0);
+
+out1:
+	libusb_close(tmp);
+
+out0:
+	return ret;
+}
+
+static libusb_device_handle *find_and_open_device(libusb_device **list,
+		ssize_t count, unsigned iSerial)
+{
+	libusb_device_handle		*udevh = NULL;
+	libusb_device			*found = NULL;
+	int				ret;
+	int				i;
+
+	for (i = 0; i < count; i++) {
+		libusb_device *udev = list[i];
+
+		ret = match_device_id(udev);
+		if (ret < 0) {
+			DBG("%s: couldn't match device id\n", __func__);
+			break;
+		}
+
+		if (iSerial) {
+			ret = match_device_serial_number(udev, iSerial);
+			if (ret < 0) {
+				DBG("%s: couldn't match serial number\n", __func__);
+				found = NULL;
+				break;
+			}
+		}
+
+		found = udev;
+		break;
+	}
+
+	ret = libusb_open(found, &udevh);
+	if (ret < 0 || !found) {
+		DBG("%s: couldn't open device\n", __func__);
+		goto out0;
+	}
+
+out0:
+	return udevh;
+}
 
 static int find_and_claim_interface(libusb_device_handle *udevh)
 {
@@ -173,12 +298,22 @@ int main(int argc, char *argv[])
 {
 	libusb_context		*context;
 	libusb_device_handle	*udevh;
+	libusb_device		**list;
+
+	unsigned		iSerial = 0;
+
+	ssize_t			count;
+
 	int			ret = 0;
+	char			*serial = NULL;
 
 	if (argc < 2) {
 		ret = -EINVAL;
 		goto out0;
 	}
+
+	if (argc == 3)
+		iSerial = strtoul(argv[2], &serial, 16);
 
 	/* initialize libusb */
 	libusb_init(&context);
@@ -186,13 +321,16 @@ int main(int argc, char *argv[])
 	/* get rid of debug messages */
 	libusb_set_debug(context, 0);
 
-	udevh = libusb_open_device_with_vid_pid(context, CLEWARE_VENDOR_ID,
-			CLEWARE_PRODUCT_ID);
-	if (!udevh) {
-		DBG("%s: couldn't find device 0x%02x:0x%02x\n", __func__,
-				CLEWARE_VENDOR_ID, CLEWARE_PRODUCT_ID);
-		ret = -ENODEV;
+	count = libusb_get_device_list(context, &list);
+	if (count < 0) {
+		DBG("%s: couldn't get device list\n", __func__);
 		goto out1;
+	}
+
+	udevh = find_and_open_device(list, count, iSerial);
+	if (!udevh) {
+		DBG("%s: couldn't find a suitable device\n", __func__);
+		goto out2;
 	}
 
 	ret = find_and_claim_interface(udevh);
@@ -213,6 +351,7 @@ out3:
 
 out2:
 	libusb_close(udevh);
+	libusb_free_device_list(list, 1);
 
 out1:
 	libusb_exit(context);
