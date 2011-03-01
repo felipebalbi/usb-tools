c5122cdfd2b0b4a7396931d00b30fa37d1dc3b22
Toni Kettula <toni.kettula@nokia.com>
2011-03-01 12:41:41 +0200

    usb-tools: serialc: Changed megabits calculation to use SI prefix M=1000*1000
    The existing code calculated megabits per second using M=1024*1024.
    Normal convention (and SI standards) measure bit-based line speeds by
    M=1000*1000, so the measurements reported by the tool were roughly 5%
    lower than the real throughput.
    
    This patch changes the calculation to use M=1000*1000 to be consistent
    with the USB standard speed units (480 Mbps is with M=1000*1000) and the
    SI standard.
    
    Signed-off-by: Toni Kettula <toni.kettula@nokia.com>
    Signed-off-by: Yauheni Kaliuta <yauheni.kaliuta@nokia.com>

--- serialc.c
+++ serialc.c
@@ -269,13 +269,15 @@ static void release_interface(struct usb_serial_test *serial)
 }
 
 /**
- * throughtput - Calculate throughput
+ * throughput - Calculate throughput in megabits per second
  * @usecs:	Number of microseconds taken
  * @size:	Size of data transfered
  */
 static float throughput(int64_t usecs, size_t size)
 {
-	return (float) size / ((usecs / 1000000.0) * 1024) * 8 / 1024 ;
+	float bits = (float) size * 8.0;
+	float secs = (float) usecs / 1000000.0;
+	return (bits / secs) / 1000000.0;
 }
 
 /**
